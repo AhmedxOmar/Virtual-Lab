a59afcd280d2dd6347856960b7a2fc19aa434bc7
2Ahmed4Omar <ahmed.w.dev01@gmail.com>
2025-04-04 23:37:41 +0200

    styling effects

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Sidebar() {
     const [topics, setTopics] = useState([]);
+    const location = useLocation();
 
     useEffect(() => {
         fetch("/topics.json")
@@ -16,21 +17,23 @@ export default function Sidebar() {
             <div className="sidebar">
                 <nav>
                     <ul>
-                        {topics.map((topic) => (
-                            <li key={topic.id} className="hover:bg-[#161616] transition ring-0 focus-outline-none cursor-pointer">
-                                <Link to={`/docs/${topic.id}`} className="" >
-                                    {topic.title}
-                                </Link>
-                            </li>
-                        ))}
+                        {topics.map((topic) => {
+                            const isActive = location.pathname === `/docs/${topic.id}`;
+                            return (
+                                <li
+                                    key={topic.id}
+                                    className={`hover:bg-[#161616] transition ring-0 focus:bg-[#161616] cursor-pointer ${isActive ? "bg-[#161616]" : ""
+                                        }`}
+                                >
+                                    <Link to={`/docs/${topic.id}`} className="">
+                                        {topic.title}
+                                    </Link>
+                                </li>
+                            );
+                        })}
                     </ul>
                 </nav>
             </div>
-
         </aside>
     );
 }
-
-
-{/* <aside className="markdownSidebar w-64 bg-[#1a1a1a] text-white h-full overflow-y-auto p-4">
- */}
